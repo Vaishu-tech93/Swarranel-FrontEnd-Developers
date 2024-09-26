a4c3cc7c16c75ebe82d2ded7a16749b4c324327a
sanika <sanikabargal7@gmail.com>
2024-09-26 11:15:22 +0530

    responsive aboutUs page added

--- src/FirstComponent/About.js
+++ src/FirstComponent/About.js
@@ -2,35 +2,53 @@ import React from 'react'
 import img from '../FirstComponent/b22.jpg';
 function About() {
   return (
-    <><div style={{backgroundImage :`URL(${img})`}} className='h-5/6'>
-    
-        <div class="mt-20 ">
-        <div class="flex">
-       
-        <h1 class=" uppercase ml-60 mt-40 mr-20 text-4xl font-extrabold text-black-900 dark:text-white overline ..."> <br></br>A B O U T   U S</h1>
-        
-      <p class ="mt-44 mb-44 ml-20 text-lg text-gray-900 dark:text-white tracking-tight text-gray-500 md:text-lg dark:text-gray-400">Founded in 2016, Swarraneel Entertainments is a brainchild of Nilesh Mate, a passionate singer,<br></br> who believes in “Hidden Talent Existence” in your immediate neighborhood. Swarraneel Entertainments is<br></br> committed to bring performing as well as non-performing arts to life by bringing quality events to the art<br></br> lovers like Musical Shows, Art Exhibitions, Professional Drama Shows, Book Publications and other such <br></br>arts. While arranging professional events, Swarraneel also eyes on bringing the fresh talent on the fore by<br></br> giving them an exposure to present their skills and talent to the best potential.<br></br><br></br>
-
-Swarraneel has, so far, produced many high quality musical events like Melodious Duo Series, Bappi Lahiri <br></br>Special, Magical Pacham, Melody Queens, Timeless Gulzar,  Marathi Special, Colorbuds…an art exhibition <br></br>exhibiting art by artists of age upto 18 years, Book Launch of poems by Mrs. Paresha Jogalekar-<br></br>Paranjape and many more such events and is working on producing some breathtaking events.<br></br><br></br><br></br>
-
-For any show inquiries or event management proposals, please feel free to send us an email<br></br> on <a href = "https://outlook.live.com/mail/0/"><span class="text-red-600">nilesh@swarraneel.in</span></a> or connect with us on +91-7522955281</p><br></br>
-</div>
-</div>
-</div>
-<div class="bg-gray-200 ">
-<div class="text-center  mb-1 mt-20">
-<h1 class="uppercase text-6xl text-gray-900 dark:text-white mt-10 pt-10 "> EVENTS SO FAR</h1>
-<p class=" text-gray-500 dark:text-gray-400 mt-10 mb-5">Take a peek inside our Wonderworld</p>
-</div>
-<div class="w-100 h-80 flex ml-20 pt-5 mr-20" >
-    <img src="/2.webp" class="mr-20"></img>
-    <img src="/111.webp"class=""></img>
+    <>
+   <div style={{ backgroundImage: `URL(${img})` }} className='h-5/6 bg-cover bg-center'>
+  <div className="mt-20">
+    <div className="flex flex-col md:flex-row items-start">
+      <h1 className="uppercase mt-10 md:mt-40 md:ml-60 mr-4 text-4xl font-extrabold text-black-900 dark:text-white">
+        A B O U T U S
+      </h1>
+      <p className="mt-10 md:mt-44 mb-10 md:mb-44 mx-4 text-lg text-gray-900 dark:text-white tracking-tight dark:text-gray-400">
+        Founded in 2016, Swarraneel Entertainments is a brainchild of Nilesh Mate, a passionate singer,
+        who believes in “Hidden Talent Existence” in your immediate neighborhood. Swarraneel Entertainments
+        is committed to bringing performing as well as non-performing arts to life by hosting quality events
+        for art lovers, such as Musical Shows, Art Exhibitions, Professional Drama Shows, Book Publications,
+        and other artistic endeavors. While arranging professional events, Swarraneel also focuses on showcasing
+        fresh talent by providing opportunities for them to present their skills to the best of their potential.
+        <br /><br />
+        Swarraneel has produced many high-quality musical events like Melodious Duo Series, Bappi Lahiri Special,
+        Magical Pacham, Melody Queens, Timeless Gulzar, Marathi Special, Colorbuds—an art exhibition showcasing
+        works by artists aged up to 18 years, Book Launch of poems by Mrs. Paresha Jogalekar-Paranjape, and many
+        more such events. They are continually working on producing breathtaking events.
+        <br /><br />
+        For any show inquiries or event management proposals, please feel free to send us an email at{" "}
+        <a href="https://outlook.live.com/mail/0/">
+          <span className="text-red-600">nilesh@swarraneel.in</span>
+        </a> or connect with us at +91-7522955281.
+      </p>
     </div>
-    <div class=" flex ml-20 mr-20 p-5">
-    <img src="/4.webp" class="mr-20 w-100 pt-10 h-80"></img>
-    <img src="/3.webp" class="w-100  h-60 m-10"></img>
+  </div>
 </div>
-    </div>
+
+<div className="bg-gray-200">
+  <div className="text-center mb-1 mt-20">
+    <h1 className="uppercase text-4xl md:text-6xl text-gray-900 dark:text-white mt-10 pt-10">
+      EVENTS SO FAR
+    </h1>
+    <p className="text-gray-500 dark:text-gray-400 mt-10 mb-5">Take a peek inside our Wonderworld</p>
+  </div>
+  <div className="flex flex-col md:flex-row justify-center mx-4 mt-5">
+    <img src="/2.webp" className="w-100 md:w-auto md:mr-4 mb-4 md:mb-0 h-50" alt="Event 1" />
+    <img src="/111.webp" className="w-100 md:w-100 h-50" alt="Event 2" />
+  </div>
+  <div className="flex flex-col md:flex-row justify-center mx-4 mt-5">
+    <img src="/4.webp" className="w-100 md:w-100 md:mr-4 mb-4 md:mb-0 h-50" alt="Event 3" />
+    <img src="/3.webp" className="w-100 md:w-100 h-50" alt="Event 4" />
+  </div>
+</div>
+
+  
     
     </> 
   )
